d33f16428cd5339b3a33aa3385e698f502e02f94
JualNasiRendang <radikaradikaff@gmail.com>
2026-07-22 05:57:38 +0700

    Update gag2.lua

--- gag2.lua
+++ gag2.lua
@@ -1,1 +1,2 @@
-ga
+getgenv().SCRIPT_KEY = "KEYLESS"
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/05dd072ce9c92776ce900fb1729563b5dd5cf6f267665b43db5021f5402f0418/download"))()
